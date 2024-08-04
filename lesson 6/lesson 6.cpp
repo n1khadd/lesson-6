1d92656597a54ea5959528076e3c4e298dc5f9d6
mirze <mirze@DESKTOP-H5NKI9E>
2024-08-05 00:21:48 +0400

    Add project files.

---
+++ lesson 6/lesson 6.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //int asc = 0;
+    //
+    //while (asc < 256){
+    //    cout << asc << " -->> " << (char)asc << endl;
+    //    asc++;
+    //}
+
+    //int i = 1;
+
+    //while (i <= 50) {
+    //    if (i % 2 == 0) {
+    //        cout << i << " ";
+    //    }
+    //    i++;
+    //}
+
+    //int i=0;
+    //int ulduz;
+    //cout << "ulduzlarin sayini qeyd edin: ";
+    //cin >> ulduz;
+    //while (i < ulduz ) {
+    //    cout << "*" << " ";
+    //    i++;
+    //}
+
+    //int i;
+    //int a;
+    //cin >> i;
+    //cin >> a;
+    //int cuteded = 0;
+    //int tekeded = 1;
+    //while (i <= a) {
+    //    if (i % 2 == 0) {
+    //    cuteded += i;
+    //    
+    //    }
+    //    else {
+    //        tekeded *= i;
+    //    }
+
+    //}
+
+        //int i;
+        //int a;
+        //cout << "başlanğıcını daxil edin: ";
+        //cin >> i;
+        //cout << "sonunu daxil edin: ";
+        //cin >> a;
+        //        
+        //int cuteded = 0;
+        //int tekeded = 1; 
+        //        
+        //int ed1 = i;
+        //      
+        //while (ed1 < a) {
+        //    if (ed1 % 2 == 0) {
+        //        
+        //        cuteded += ed1;
+        //    }
+        //    else {
+        //        
+        //        tekeded *= ed1;
+        //    }
+        //    
+        //    ed1++;
+        //}
+        //cout << "Cüt ədədlərin cəmi: " << cuteded << endl;
+        //cout << "Tək ədədlərin hasil: " << tekeded << endl;
+
+   
+
+
+
+	
+}
